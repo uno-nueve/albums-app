1330339c768d610767a152722c2cc719d479637a
Luciano Montilla <unonueve95@gmail.com>
2024-12-03 17:00:23 -0300

    Update Header.jsx

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,21 @@
 import { Link, NavLink } from "react-router";
 import styled from "styled-components";
 
-const HeaderWrapper = styled.header`
+const HeaderWrapper = styled.div`
     border: 1px solid black;
     background-color: white;
     color: black;
     padding: 18px 40px;
     height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const HeaderContainer = styled.header`
+    max-width: 1920px;
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -20,14 +29,16 @@ const NavWrapper = styled.nav`
 export const Header = ({ navLinks }) => {
     return (
         <HeaderWrapper>
-            <Link to="/">LOGO</Link>
-            <NavWrapper>
-                {navLinks?.map(({ label, to }) => (
-                    <NavLink key={to} to={to}>
-                        {label}
-                    </NavLink>
-                ))}
-            </NavWrapper>
+            <HeaderContainer>
+                <Link to="/">LOGO</Link>
+                <NavWrapper>
+                    {navLinks?.map(({ label, to }) => (
+                        <NavLink key={to} to={to}>
+                            {label}
+                        </NavLink>
+                    ))}
+                </NavWrapper>
+            </HeaderContainer>
         </HeaderWrapper>
     );
 };
