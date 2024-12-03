c01d6442626c33c9a3cc0ef2ff64a1ac5cc04272
Luciano Montilla <unonueve95@gmail.com>
2024-12-03 18:03:28 -0300

    Update Home route

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router";
 import { ArrowNarrowRight } from "../components/ui/svgs";
 
 const InlineButton = styled(Button)`
-    width: 50%;
+    max-width: 320px;
     justify-content: space-between;
     font-size: 20px;
 `;
@@ -14,25 +14,67 @@ const InlineButton = styled(Button)`
 const DisplayCol = styled(GridCol)`
     justify-content: space-between;
     padding: 80px 40px;
+    align-items: end;
+
+    @media (max-width: 768px) {
+        grid-template-columns: minmax(0, 1fr);
+        background-image: url("https://images.unsplash.com/photo-1722868453565-dfa553dacc95?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    }
+`;
+
+const ImageCol = styled(GridCol)`
+    padding: 0;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+const GridLayout = styled(GridColsWrapper)`
+    @media (max-width: 768px) {
+        grid-template-columns: minmax(0, 1fr);
+    }
+`;
+
+const StyledLink = styled(Link)`
+    width: 100%;
+    display: flex;
+    justify-content: end;
+`;
+
+const ButtonIcon = styled.div`
+    width: 24px;
+    height: 100%;
+`;
+
+const BgImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `;
 
 export const Home = () => {
     return (
         <>
-            <GridColsWrapper cols="repeat(2, minmax(0, 1fr))">
-                <GridCol>img</GridCol>
+            <GridLayout cols="repeat(2, minmax(0, 1fr))">
+                <ImageCol>
+                    <BgImage
+                        src="https://images.unsplash.com/photo-1722868453565-dfa553dacc95?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        alt="abstract blob"
+                    />
+                </ImageCol>
                 <DisplayCol>
-                    <h1 style={{ textAlign: "right" }}>Hola Mundo</h1>
-                    <Link to="/albums" style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <h1>Hola Mundo</h1>
+                    <StyledLink to="/albums">
                         <InlineButton>
                             Comprar
-                            <div style={{ width: "24px", display: "flex", alignItems: "center" }}>
+                            <ButtonIcon>
                                 <ArrowNarrowRight />
-                            </div>
+                            </ButtonIcon>
                         </InlineButton>
-                    </Link>
+                    </StyledLink>
                 </DisplayCol>
-            </GridColsWrapper>
+            </GridLayout>
         </>
     );
 };
