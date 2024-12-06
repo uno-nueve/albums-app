6b0ffd50af5c2c9b79513cc060c3c92ae2d3def0
Luciano Montilla <unonueve95@gmail.com>
2024-12-06 18:35:37 -0300

    Update Login page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import { Login } from "./routes/login.jsx";
+import { Login } from "./routes/Login.jsx";
 import { Dashboard } from "./routes/Dashboard.jsx";
 import { Catalog } from "./routes/Catalog.jsx";
 import { Orders } from "./routes/Orders.jsx";
 import { Store } from "./routes/Store.jsx";
 import { Returns } from "./routes/Returns.jsx";
 import { Home } from "./routes/Home.jsx";
+import { Logout } from "./routes/Logout.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
@@ -21,14 +22,15 @@ createRoot(document.getElementById("root")).render(
                         <Route index element={<Catalog />} />
                     </Route>
                     <Route path="returns" element={<Returns />} />
+                    <Route path="dashboard" element={<Dashboard />}>
+                        <Route index element={<Catalog />} />
+                        <Route index path="catalog" element={<Catalog />} />
+                        <Route path="orders" element={<Orders />} />
+                    </Route>
                 </Route>
 
                 <Route path="login" element={<Login />} />
-                <Route path="dashboard" element={<Dashboard />}>
-                    <Route index element={<Catalog />} />
-                    <Route index path="catalog" element={<Catalog />} />
-                    <Route path="orders" element={<Orders />} />
-                </Route>
+                <Route path="logout" element={<Logout />} />
             </Routes>
         </BrowserRouter>
     </StrictMode>
---
+++ src/utils/mockUsers.js
@@ -0,0 +1,26 @@
+export const mockUsers = [
+    {
+        username: "Juan Perez",
+        avatar: "https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Caleb",
+        credentials: {
+            email: "juan.perez@example.com",
+            password: "password123",
+        },
+    },
+    {
+        username: "Maria Gomez",
+        avatar: "https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Alexander",
+        credentials: {
+            email: "maria.gomez@example.com",
+            password: "password123",
+        },
+    },
+    {
+        username: "Carlos Lopez",
+        avatar: "https://api.dicebear.com/9.x/bottts-neutral/svg?seed=Chase",
+        credentials: {
+            email: "carlos.lopez@example.com",
+            password: "password123",
+        },
+    },
+];
