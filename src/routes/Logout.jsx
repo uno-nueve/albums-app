9101c8d162ad4f12803384d9bbad3393edaa5a77
Luciano Montilla <unonueve95@gmail.com>
2024-12-06 18:38:45 -0300

    Create Logout page

---
+++ src/routes/Logout.jsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+export const Logout = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        localStorage.removeItem("session");
+        return navigate("/login");
+    }, []);
+};
