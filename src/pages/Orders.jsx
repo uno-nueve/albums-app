801459f0130dbea3b0347c244be6b121b4bb9837
Luciano Montilla <unonueve95@gmail.com>
2024-12-09 17:18:05 -0300

    Update Orders page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,3 +1,49 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { baseUrl, sales } from "../App";
+
 export const Orders = () => {
-    return <div>Orders</div>;
+    const [error, setError] = useState();
+    const [isLoading, setisLoading] = useState(false);
+    const [data, setData] = useState([]);
+    console.log(data);
+
+    const getSales = async () => {
+        setisLoading(true);
+
+        try {
+            const res = await axios.get(`${baseUrl}${sales}`);
+            setData(res.data);
+            // return res.data;
+        } catch (e) {
+            setError(e);
+            console.error(e);
+        } finally {
+            setisLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getSales();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Something went wrong. Please try again</div>;
+    }
+
+    return (
+        <div>
+            <ul>
+                {data.map(({ _id, ordenNumero, nombreCliente, monto }) => (
+                    <li key={_id}>
+                        #{ordenNumero} | {nombreCliente} | ${monto}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
