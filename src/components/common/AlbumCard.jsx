060652d6f8c4eb25cd33552c4a89f726b65e996c
Luciano Montilla <unonueve95@gmail.com>
2024-12-09 17:17:42 -0300

    Update Catalog page

--- src/components/common/AlbumCard.jsx
+++ src/components/common/AlbumCard.jsx
@@ -1,6 +1,6 @@
 export const AlbumCard = ({ titulo, artista, imagen }) => {
     return (
-        <div style={{ width: "100%" }}>
+        <div style={{ width: "100%", height: "100%" }}>
             <img src={imagen.url} alt={titulo} style={{ width: "100%" }} />
             <p>{titulo}</p>
             <p>{artista}</p>
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -3,27 +3,44 @@ import { useEffect, useState } from "react";
 import { albums, baseUrl } from "../App";
 import { AlbumCard } from "../components/common/AlbumCard";
 import GridColsWrapper from "../components/ui/GridColWrapper";
-import GridCol from "../components/ui/GridCol";
+import { useDispatch, useSelector } from "react-redux";
+import { indexAlbums } from "../state/albums/albumsSlice";
+import styled from "styled-components";
+
+const GridLayout = styled(GridColsWrapper)`
+    min-height: calc(100% - 40px);
+`;
 
 export const Catalog = () => {
     const [error, setError] = useState();
     const [isLoading, setisLoading] = useState(false);
-    const [data, setData] = useState([]);
+    // const [data, setData] = useState([]);
+    const dispatch = useDispatch();
+    const data = useSelector((state) => state.albums);
+
+    const getAlbums = async () => {
+        setisLoading(true);
+
+        try {
+            const res = await axios.get(`${baseUrl}${albums}`);
+            // setData(res.data);
+            return res.data;
+        } catch (e) {
+            setError(e);
+            console.error(e);
+        } finally {
+            setisLoading(false);
+        }
+    };
+
+    const resolve = async () => {
+        const response = await getAlbums();
+
+        dispatch(indexAlbums(response));
+    };
 
     useEffect(() => {
-        const getAlbums = async () => {
-            setisLoading(true);
-
-            try {
-                const res = await axios.get(`${baseUrl}${albums}`);
-                setData(res.data);
-            } catch (e) {
-                setError(e);
-            } finally {
-                setisLoading(false);
-            }
-        };
-        getAlbums();
+        resolve();
     }, []);
 
     if (isLoading) {
@@ -35,12 +52,10 @@ export const Catalog = () => {
     }
 
     return (
-        <GridColsWrapper cols="repeat(4, minmax(0, 1fr))">
-            <GridCol>
-                {data.map(({ _id, titulo, artista, images }) => (
-                    <AlbumCard key={_id} titulo={titulo} artista={artista} imagen={images[0]} />
-                ))}
-            </GridCol>
-        </GridColsWrapper>
+        <GridLayout cols="repeat(4, minmax(0, 1fr))">
+            {data.map(({ _id, titulo, artista, images }) => (
+                <AlbumCard key={_id} titulo={titulo} artista={artista} imagen={images[0]} />
+            ))}
+        </GridLayout>
     );
 };
