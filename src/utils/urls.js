6f6efadf65f9b1d5655bfa2758c9c05fa831efd4
Luciano Montilla <unonueve95@gmail.com>
2024-12-10 15:48:06 -0300

    Create urls export file

---
+++ src/utils/urls.js
@@ -0,0 +1,6 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+export const ALBUMS = import.meta.env.VITE_ALBUMS;
+export const SALES = import.meta.env.VITE_SALES;
+export const SEARCH_ALBUM = import.meta.env.VITE_SEARCH;
+export const BUY_ALBUM = import.meta.env.VITE_BUY;
+export const RETURN_ALBUM = import.meta.env.VITE_RETURN;
