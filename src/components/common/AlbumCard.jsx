5e31c77cb4e3eef197abb7f17f68e7a0fdf6e070
Luciano Montilla <unonueve95@gmail.com>
2024-12-14 12:48:39 -0300

    Update AlbumCard

--- src/components/common/AlbumCard.jsx
+++ src/components/common/AlbumCard.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
 import Button from "../ui/Button";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../state/cart/cartSlice";
@@ -6,7 +6,7 @@ import { useContext } from "react";
 import { CartContext } from "../../contexts/CartContext";
 
 export const AlbumCard = ({ album }) => {
-    const { titulo, artista, images } = album;
+    const { titulo, artista, images, _id } = album;
     const { setShowModal } = useContext(CartContext) || {};
     const location = useLocation();
     const dispatch = useDispatch();
@@ -16,14 +16,22 @@ export const AlbumCard = ({ album }) => {
         dispatch(addToCart(album));
     };
 
+    if (location.pathname === "/albums") {
+        return (
+            <Link to={`/albums/${_id}`} style={{ width: "100%", height: "100%" }}>
+                <img src={images[0].url} alt={titulo} style={{ width: "100%" }} />
+                <p>{titulo}</p>
+                <p>{artista}</p>
+                <Button onClick={handleCart}>Añadir al carrito</Button>
+            </Link>
+        );
+    }
+
     return (
-        <div style={{ width: "100%", height: "100%" }}>
+        <Link to={`/dashboard/catalog/${_id}`} style={{ width: "100%", height: "100%" }}>
             <img src={images[0].url} alt={titulo} style={{ width: "100%" }} />
             <p>{titulo}</p>
             <p>{artista}</p>
-            {location.pathname === "/albums" && (
-                <Button onClick={handleCart}>Añadir al carrito</Button>
-            )}
-        </div>
+        </Link>
     );
 };
